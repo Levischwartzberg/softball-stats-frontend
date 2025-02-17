2937e55598835e57430491976fd4f8cb80238d96
N1TVYGK <schwartzberglevi@johndeere.com>
2025-02-17 16:18:01 -0600

    Configured UserPool and automatic guest login based on env based credentials. Integrated first lambda connection by using saved token from login to attach to call to API gateway with jwt authorization.

--- src/pages/SoftballStatsApp.tsx
+++ src/pages/SoftballStatsApp.tsx
@@ -1,8 +1,10 @@
+import React, {useState, useEffect} from "react";
 import {createBrowserRouter, RouterProvider, useNavigate, useLocation, Outlet} from "react-router-dom";
-import React from "react";
 import {routes} from "../routes";
-import TopMenu from "../components/TopMenu/TopMenu";
 import menuItems from "../menuItems";
+import TopMenu from "../components/TopMenu/TopMenu";
+import {CognitoUser, AuthenticationDetails} from "amazon-cognito-identity-js";
+import UserPool from "../UserPool";
 
 const Layout = () => {
 
@@ -19,7 +21,39 @@ const Layout = () => {
     </>
 }
 
+const guestUsername = process.env.REACT_APP_GUEST_USERNAME as string;
+const guestPassword = process.env.REACT_APP_GUEST_USERNAME as string;
+
 const SoftballStatsApp : React.FC = () => {
+
+    const [username, setUsername] = useState(guestUsername);
+    const [password, setPassword] = useState(guestPassword);
+
+    useEffect(() => {
+
+        const user = new CognitoUser({
+            Username : username,
+            Pool : UserPool
+        })
+
+        const authDetails = new AuthenticationDetails({
+            Username : username,
+            Password : password
+        })
+
+        user.authenticateUser(authDetails, {
+            onSuccess : (data) => {
+                console.log("Success: ", data);
+                localStorage.setItem("userAccessToken", data.getIdToken().getJwtToken());
+            },
+            onFailure : (data) => {
+                console.error("Error: ", data);
+            },
+            newPasswordRequired : (data) => {
+                console.log(data);
+            }
+        })
+    },[username, password]);
 
     const router = createBrowserRouter([
         {
