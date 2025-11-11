ffe778c2abe412b99c83865efb94f67e312ccbc7
N1TVYGK <schwartzberglevi@johndeere.com>
2025-11-11 00:48:51 -0600

    Added game info display stuff

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -5,6 +5,7 @@ import AsyncStateWrapper, {QueryState} from "@/components/common/AsyncStateWrapp
 import {useLazyGetScoresheetQuery} from "@/store/scoresheet/scoresheetApiSlice";
 import Button from "@mui/material/Button";
 import Scoresheet from "@/components/Scoresheet/Scoresheet";
+import GameInfoCard from "@/components/GameInfoTable/GameInfoCard";
 import LineScore from "@/components/LineScore/LineScore";
 
 const ResultPage = () => {
@@ -14,37 +15,9 @@ const ResultPage = () => {
     const getBoxscoreQuery = useGetBoxscoreQuery(parseInt(gameId!));
     const [getScoresheetTrigger, getScoresheetQuery] = useLazyGetScoresheetQuery();
 
-    const formatScore = (runsFor : number, runsAgainst : number) : string => {
-
-        if (runsFor > runsAgainst) {
-            return `${runsFor} - ${runsAgainst}`;
-        } else {
-            return `${runsAgainst} - ${runsFor}`;
-        }
-    }
-
-    const determineResult = (runsFor : number, runsAgainst : number) : string => {
-
-        if (runsFor > runsAgainst) {
-            return "Win";
-        } else if (runsFor < runsAgainst) {
-            return "Loss";
-        }
-        return "Tie";
-    }
-
     return <div className="content">
         <AsyncStateWrapper query={getBoxscoreQuery as QueryState} >
-            {getBoxscoreQuery.data && (
-                <div>
-                    <h2 style={{textAlign: "left"}}>
-                        <span style={{fontWeight: "bold"}}> Game Date: </span> <span style={{fontWeight: "normal"}}> {getBoxscoreQuery.data!.gameInfo.date.toString()} </span>
-                    </h2>
-                    <h2 style={{textAlign: "left"}}>
-                        <span style={{fontWeight: "bold"}}> Result: </span> <span style={{fontWeight: "normal"}}> {determineResult(getBoxscoreQuery.data!.gameInfo.runsFor, getBoxscoreQuery.data!.gameInfo.runsAgainst) + " " + formatScore(getBoxscoreQuery.data!.gameInfo.runsFor, getBoxscoreQuery.data!.gameInfo.runsAgainst)} </span>
-                    </h2>
-                </div>
-            )}
+            <GameInfoCard boxscoreDTO={getBoxscoreQuery.data!} />
 
             <LineScore boxscoreDTO={getBoxscoreQuery.data!} />
 
